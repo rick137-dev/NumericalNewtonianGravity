f92a7347e12bd52d024253c069ac6c77610167f0
rick137-dev <caiulorick137@gmail.com>
2025-09-23 21:27:46 +0200

    Added Solver class with skeleton code

---
+++ Solver.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy
+
+
+class SystemSolver:
+
+    def solveEulerMethod(self):
+        pass
+
+    def solveRungeKuttaExplicit(self):
+        pass
+
+    def solveRungeKuttaScipy(self):
+        pass
+
+
+
