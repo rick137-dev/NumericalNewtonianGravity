731c033b24e1cc57a7993bac43c5ff0bfec34796
rick137-dev <caiulorick137@gmail.com>
2025-09-23 20:54:32 +0200

    Initial commit

---
+++ Star.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Star:
+
+    def __init__(self, initial_position, initial_velocity, mass):
+        self.initial_position = initial_position
+        self.initial_velocity = initial_velocity
+        self.mass = mass
+
+
+    def getMass(self):
+        return self.mass
+
+    def changeMass(self, mass):
+        self.mass = mass
+
+    def setPosition(self, position):
+        self.initial_position = position
+
+    def setVelocity(self, velocity):
+        self.initial_velocity = velocity
+
+
