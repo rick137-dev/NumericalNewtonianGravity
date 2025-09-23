0985da3c732d36c5ae5e8035dd5832a894cb4603
rick137-dev <caiulorick137@gmail.com>
2025-09-23 22:17:32 +0200

    Added merge distance for possibility of having stars merge

--- StarSystem.py
+++ StarSystem.py
@@ -3,13 +3,18 @@ from Star import Star
 
 class StarSystem:
 
-    def __init__(self):
+    def __init__(self, merge_distance =0):
         self.stars = []
         self.star_count =0
         self.system_trajectories = None
+        self.merge_distance = merge_distance
 
     def getStars(self):
         return self.stars
+
+    def setMergeDistance(self, distance):
+        self.merge_distance = distance
+
 
     def getStarCount(self):
         return self.star_count
