987e53c80889202e74b01d32abab708c904a9bf1
rick137-dev <caiulorick137@gmail.com>
2025-09-27 18:10:34 +0200

    Removed merge_distance capability and removed star_count parameter

--- StarSystem.py
+++ StarSystem.py
@@ -3,11 +3,9 @@ from Star import Star
 
 class StarSystem:
 
-    def __init__(self, merge_distance =0):
+    def __init__(self):
         self.stars = []
-        self.star_count =0
         self.system_trajectories = None
-        self.merge_distance = merge_distance
 
     def getStars(self):
         return self.stars
@@ -15,16 +13,15 @@ class StarSystem:
     def setMergeDistance(self, distance):
         self.merge_distance = distance
 
-
-    def getStarCount(self):
-        return self.star_count
-
     def addStar(self, star: Star):
         self.stars.append(star)
 
     def addStars(self, starList):
         for star in starList:
             self.stars.append(star)
+
+    def getStarCount(self):
+        return len(self.stars)
 
     def calculateTrajectories(self, method = "RKScipy"):
         pass
