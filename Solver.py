daa19529312d451279ceb9610416cc94cb20d3b0
rick137-dev <caiulorick137@gmail.com>
2025-09-28 22:52:19 +0200

    Added assert to ensure only a possible method is chosen, added masses calculation for each solution method and added RKScipy method solver

--- Solver.py
+++ Solver.py
@@ -5,17 +5,26 @@ import scipy
 class SystemSolver:
 
     @staticmethod
-    def generalSolver(starSystem, step_size, end_time, method = "RKScipy"):
+    def generalSolver(starSystem, step_size, end_time, method = "RKScipy", absolute_tolerance = 1e-6, relative_tolerance = 1e-3):
+        assert method == "RkScipy" or method == "Euler" or method =="Midpoint" , "Method name not found!"
+
         if method == "Euler":
             return SystemSolver.solveEulerMethod(starSystem,step_size,end_time)
+        elif method == "Midpoint":
+            return SystemSolver.solveMidpointMethod(starSystem,step_size,end_time)
         else:
-            return None
+            return SystemSolver.solveRungeKuttaScipy(starSystem, step_size, end_time,absolute_tolerance, relative_tolerance)
 
 
     @staticmethod
     def solveEulerMethod(starSystem, step_size, end_time):
         trajectory = []
         Y_0 = []
+
+        masses = []
+
+        for star in starSystem.getStars():
+            masses.append(star.getMass())
 
         for star in starSystem.getStars():
             pos_x , pos_y = star.getPosition()
@@ -37,18 +46,23 @@ class SystemSolver:
 
         for time in times[:-1]:
             Y = trajectory[-1]
-            Y_new = Y + step_size*starSystem.getSystemDerivative(time, Y)
+            Y_new = Y + step_size*starSystem.getSystemDerivative(time, Y, masses)
             trajectory.append(Y_new)
 
         return times , trajectory
 
 
 
-
+    #This method uses 1 midpoint, so it is equivalent to Runge Kutta 2
     @staticmethod
     def solveMidpointMethod(starSystem, step_size, end_time):
         trajectory = []
         Y_0 = []
+
+        masses = []
+
+        for star in starSystem.getStars():
+            masses.append(star.getMass())
 
         for star in starSystem.getStars():
             pos_x, pos_y = star.getPosition()
@@ -68,19 +82,43 @@ class SystemSolver:
 
         for time in times[:-1]:
             Y = trajectory[-1]
-            slope_start = starSystem.getSystemDerivative(time,Y)
+            slope_start = starSystem.getSystemDerivative(time,Y, masses)
             time_midpoint = time + step_size//2
             Y_midpoint = Y + (step_size *slope_start)//2
-            slope_midpoint = starSystem.getSystemDerivative(time_midpoint, Y_midpoint)
+            slope_midpoint = starSystem.getSystemDerivative(time_midpoint, Y_midpoint, masses)
             Y_new = Y + step_size *slope_midpoint
             trajectory.append(Y_new)
 
 
         return times, trajectory
 
+    #Scipy solve_ivp uses by default Runge Kutta 4-5, using standard order 5 and an extra order for adaptive step size
+    @staticmethod
+    def solveRungeKuttaScipy(starSystem, step_size, end_time,absolute_tolerance, relative_tolerance):
 
-    def solveRungeKuttaScipy(self):
-        pass
+        Y_0 = []
+        masses = []
+
+        for star in starSystem.getStars():
+            masses.append(star.getMass())
+
+        for star in starSystem.getStars():
+            pos_x, pos_y = star.getPosition()
+            Y_0.append(pos_x)
+            Y_0.append(pos_y)
+
+        for star in starSystem.getStars():
+            vel_x, vel_y = star.getVelocity()
+            Y_0.append(vel_x)
+            Y_0.append(vel_y)
+
+        Y_0 = np.array(Y_0, dtype=float)
+
+        return scipy.integrate.solve_ivp(fun=lambda t, y: starSystem.getSystemDerivative(t, y, masses),t_span = [0,end_time],y0 = Y_0)
+
+
+
+
 
 
 
