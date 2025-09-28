0d6e6d5601dfba99dc9b336c9d3c536a996b2e96
rick137-dev <caiulorick137@gmail.com>
2025-09-28 17:16:35 +0200

    Added logic for the generalSolver and the full code for the Euler method solver

--- Solver.py
+++ Solver.py
@@ -4,9 +4,47 @@ import scipy
 
 class SystemSolver:
 
+    @staticmethod
+    def generalSolver(starSystem, step_size, end_time, method = "RKScipy"):
+        if method == "Euler":
+            return SystemSolver.solveEulerMethod(starSystem,step_size,end_time)
+        else:
+            return None
 
-    def solveEulerMethod(self, gradientFunction):
-        pass
+
+    @staticmethod
+    def solveEulerMethod(starSystem, step_size, end_time):
+        trajectory = []
+        Y_0 = []
+
+        for star in starSystem.getStars():
+            pos_x , pos_y = star.getPosition()
+            Y_0.append(pos_x)
+            Y_0.append(pos_y)
+
+        for star in starSystem.getStars():
+            vel_x,vel_y = star.getVelocity()
+            Y_0.append(vel_x)
+            Y_0.append(vel_y)
+
+
+        Y_0 = np.array(Y_0,dtype = float)
+
+        trajectory.append(Y_0)
+
+        times = np.arange(0, end_time + step_size, step_size)
+
+
+        for time in times[:-1]:
+            Y = trajectory[-1]
+            Y_new = Y + step_size*starSystem.getSystemDerivative(time, Y)
+            trajectory.append(Y_new)
+
+        return times , trajectory
+
+
+
+
 
     def solveMidpointMethod(self, gradientFunction):
         pass
