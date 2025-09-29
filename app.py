c4d086347518794e6585537602d0d54e0bb1ea19
rick137-dev <caiulorick137@gmail.com>
2025-09-29 20:42:24 +0200

    small edits

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from StarSystem import StarSystem
 from Solver import SystemSolver
 from Star import Star
-import pygame as pg
+import pygame
 
 
 def startApp():
     print("App opened")
+    pygame.init()
+    dt =0
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+    game_time_limit = 2000
+
+    while running and dt < game_time_limit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        clock.tick(60)
+        print("")
+
+    pygame.quit()
