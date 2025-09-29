144d580cb6e35944059c89180e85b8c242748cf2
rick137-dev <caiulorick137@gmail.com>
2025-09-29 15:45:49 +0200

    Added parameters and methods to handle merging stars that get within a minimum distance

--- StarSystem.py
+++ StarSystem.py
@@ -6,11 +6,12 @@ from Star import Star
 
 class StarSystem:
 
-    def __init__(self, gravitational_constant = 1, distance_epsilon = 0):
+    def __init__(self, gravitational_constant = 1, distance_epsilon = 0, merge_distance = 1e-4):
         self.stars = []
         self.system_trajectories = None
         self.gravitational_constant = gravitational_constant
         self.distance_epsilon = distance_epsilon
+        self.merge_distance = merge_distance
 
     def getStars(self):
         return self.stars
@@ -25,6 +26,12 @@ class StarSystem:
     def getStarCount(self):
         return len(self.stars)
 
+    @staticmethod
+    def getDistance(pos_1, pos_2):
+        x1, y1 = pos_1
+        x2, y2 = pos_2
+        return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
 
     def getCubedDistance(self,pos_1, pos_2):
         x1 , y1 = pos_1
@@ -34,13 +41,76 @@ class StarSystem:
         distance = np.sqrt(((x1-x2)**2) + ((y1-y2)**2) + (self.distance_epsilon**2))
         return distance**3
 
+    @staticmethod
+    def mergeStars(star1_position, star1_velocity,star2_position, star2_velocity, mass_1, mass_2):
+        new_star1_mass = mass_1 + mass_2
+        new_star2_mass = float(0)
+
+        new_x = (mass_1 * star1_position[0] + mass_2 * star2_position[0]) / (mass_1 + mass_2)
+        new_y = (mass_1 * star1_position[1] + mass_2 * star2_position[1]) / (mass_1 + mass_2)
+        new_star1_position = [new_x, new_y]
+
+        new_star2_position = star2_position
+
+        new_star1_velocity = [(mass_1*star1_velocity[0] + mass_2*star2_velocity[0])/new_star1_mass,(mass_1*star1_velocity[1] + mass_2*star2_velocity[1])/new_star1_mass]
+        new_star2_velocity = [0,0]
+
+        return new_star1_position, new_star2_position, new_star1_velocity, new_star2_velocity, new_star1_mass, new_star2_mass
 
 
 
-    def getSystemDerivative(self, time, Y, masses):
+    #Here we assume Y and masses are already numpy
+    def enforceMergeEvent(self, time, Y: np.array, masses : np.array):
+        N = Y.size
+        L = N // 2
 
-        masses = np.array(masses, dtype=float)
-        Y = np.array(Y, dtype=float)
+        Y_pos = Y[:L].copy()
+        Y_velocities = Y[L:].copy()
+
+        mergedStars = False
+
+        for i in range(0,L,2):
+            pos_i = [Y_pos[i], Y_pos[i + 1]]
+            vel_i = [Y_velocities[i], Y_velocities[i + 1]]
+            mass_i = masses[i//2]
+
+            for j in range(i+1,L,2):
+                pos_j = [Y_pos[j], Y_pos[j + 1]]
+                vel_j = [Y_velocities[j], Y_velocities[j + 1]]
+                mass_j = masses[j // 2]
+
+                distance = StarSystem.getDistance(pos_i,pos_j)
+
+                if distance < self.merge_distance and mass_i >0 and mass_j >0:
+                    mergedStars = True
+                    new_star1_position, new_star2_position, new_star1_velocity, new_star2_velocity, new_star1_mass, new_star2_mass = StarSystem.mergeStars(pos_i,vel_i,pos_j,vel_j,mass_i,mass_j)
+
+                    Y[i], Y[i+1] = new_star1_position
+                    Y[j], Y[j+1] = new_star2_position
+
+                    Y[i+L] , Y[i+1+L] = new_star1_velocity
+                    Y[j + L], Y[j + 1 + L] = new_star2_velocity
+
+                    masses[i//2] = new_star1_mass
+                    masses[j//2] = new_star2_mass
+
+
+                    #Refresh copied variables
+                    pos_i = [Y_pos[i], Y_pos[i + 1]]
+                    vel_i = [Y_velocities[i], Y_velocities[i + 1]]
+                    mass_i = masses[i // 2]
+
+
+
+
+        return mergedStars
+
+
+
+
+
+
+    def getSystemDerivative(self, time, Y : np.array, masses : np.array):
 
         N = Y.size
 
